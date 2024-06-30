22d5ebfb8f45c038bb3285d80aee496a0b256a6a
Елена Карпова <karpova.el.m@gmail.com>
2024-06-30 14:14:40 +0200

    initial commit

---
+++ birthday/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'birthday'
+
+urlpatterns = [
+    path('', views.birthday, name='create'),
+    path('list/', views.birthday_list, name='list'),
+    path('<int:pk>/edit/', views.birthday, name='edit'),
+]
